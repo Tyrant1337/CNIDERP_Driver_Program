680c8d246ede6995bcb75ce8557527108f751987
erikfong <erikfong@gmail.com>
2019-05-08 21:29:09 -0600

    fixing tournament mode

--- CNIDERP Driver Program.py
+++ CNIDERP Driver Program.py
@@ -12,63 +12,54 @@ from sys import executable, argv
 
 class Game:
     def __init__(self, height, width, goal, playerProgram1, playerProgram2):
-        # pass as argument for player program file
-        # self.playerfile = playerfile
-        self.gBoard = Board(height, width, goal)
+        self.gBoard = board(height, width, goal)
         self.height = height
         self.width = width
         self.goal = goal
-        self.playerProgram1 = playerProgram1
-        self.playerProgram2 = playerProgram2
         self.player1 = Popen(
-            [executable, self.playerProgram1, "1", "1", str(self.width), str(self.width), str(self.height),
-             str(self.height)],
-            stdin=PIPE, stdout=PIPE, stderr=p1error)
+                             [executable, playerProgram1, "1", "1", str(self.width), str(self.width), str(self.height),
+                              str(self.height)],
+                             stdin=PIPE, stdout=PIPE, stderr=p1error)
+            
+                             self.player2 = Popen(
+                                                  [executable, playerProgram2, "2", "2", str(self.width), str(self.width), str(self.height),
+                                                   str(self.height)],
+                                                  stdin=PIPE, stdout=PIPE, stderr=p2error)
 
-        self.player2 = Popen(
-            [executable, self.playerProgram2, "2", "2", str(self.width), str(self.width), str(self.height),
-             str(self.height)],
-            stdin=PIPE, stdout=PIPE, stderr=p2error)
 
-
-
-    # return 1 if player 1 wins, 2 if player 2 wins, 0 if game continues, 3 if no moves left
-    def playTurn(self, playerNum):
-        # get the move from the player (currently just randomly generates a valid column)
-        colIndex = self.getMove(playerNum)
-
-        # add the move to the board, which returns the row it was placed
-        rowIndex = self.gBoard.makeMove(colIndex, playerNum)
-
+# return 1 if player 1 wins, 2 if player 2 wins, 0 if game continues, 3 if no moves left
+def playTurn(self, playerNum):
+    # get the move from the player (currently just randomly generates a valid column)
+    colIndex = self.getMove(playerNum)
+    # add the move to the board, which returns the row it was placed
+    rowIndex = self.gBoard.makeMove(colIndex, playerNum)
         # print the board to the screen
         self.gBoard.printBoard()
-
         # check if there is a win
         if self.gBoard.checkIfWon(rowIndex, colIndex, playerNum):
             # indicate the player won
             return playerNum
-        # if the latest placed piece is in the top row, check if grid is full
-        elif rowIndex == 0:
-            if self.gBoard.checkIfFull():
-                # indicate a cats game
-                return 3
+    # if the latest placed piece is in the top row, check if grid is full
+    elif rowIndex == 0:
+        if self.gBoard.checkIfFull():
+            # indicate a cats game
+            return 3
         else:
             # indicate there is no win yet
             return 0
 
 
-    # this is called once to play the game
-    def playGame(self):
-        # initalize turnCode
-        turnCode = 0
-
+# this is called once to play the game
+def playGame(self):
+    # initalize turnCode
+    turnCode = 0
+        
         # player 1 goes first
         turn = 1
         # continuously loop until game ends
         while (True):
             # play the turn, which returns the turn code
             turnCode = self.playTurn(turn)
-
             # if it's 1 or 2, that player won
             if turnCode == 1:
                 print("Player 1 wins")
@@ -86,42 +77,43 @@ class Game:
                     turn = 2
                 else:
                     turn = 1
+                        
+                        
+                        # this gets the move from the player whose turn it is
+                        # right now it just generates a random valid column number
+                        # uses checkIfValid to make sure move is valid
+                        def getMove(self, turn):
+                            
+                            players = self.player1, self.player2
+                                turn -= 1
+                                    
+                                    while (True):
+                                        # send current board to player on stdout
+                                        myboard = json.dumps(self.gBoard.grid)
+                                        myboard = (myboard + '\n').encode("utf-8")
+                                        players[turn].stdin.write(myboard)
+                                        players[turn].stdin.flush()
+
+                                        # get move from player program
+                                        move = json.loads(players[turn].stdout.readline())
+                                            move = int(move["move"])
+                                            print("Player " + str(turn + 1) + " move: " + str(move))
+                                        
+                                            # check if move is valid, return move if it's good
+                                            if self.checkIfValid(move):
+                                                return move
+                                                    
+                                                    
+                                                    # tests whether move sent to driver is valid
+                                                    # returns true if valid and false otherwise
+                                                    def checkIfValid(self, column):
+                                                        # if an inbounds column and if that columns' highest row is unoccupied
+                                                        if column < self.gBoard.width and column >= 0 and self.gBoard.grid["grid"][column][0] == 0:
+                                                            return True
+                                                                return False
 
 
-    # this gets the move from the player whose turn it is
-    # right now it just generates a random valid column number
-    # uses checkIfValid to make sure move is valid
-    def getMove(self, turn):
-        players = self.player1, self.player2
-        turn -= 1
-
-        while (True):
-            # send current board to player on stdout
-            myboard = json.dumps(self.gBoard.grid)
-            myboard = (myboard + '\n').encode("utf-8")
-            players[turn].stdin.write(myboard)
-            players[turn].stdin.flush()
-
-            # get move from player program
-            move = json.loads(players[turn].stdout.readline())
-            move = int(move["move"])
-            print("Player " + str(turn + 1) + " move: " + str(move))
-
-            # check if move is valid, return move if it's good
-            if self.checkIfValid(move):
-                return move
-
-
-    # tests whether move sent to driver is valid
-    # returns true if valid and false otherwise
-    def checkIfValid(self, column):
-        # if an inbounds column and if that columns' highest row is unoccupied
-        if column < self.gBoard.width and column >= 0 and self.gBoard.grid["grid"][column][0] == 0:
-            return True
-        return False
-
-
-class Board:
+class board:
     # initalizes board
     # height = # of rows
     # width = # of columns
@@ -129,11 +121,15 @@ class Board:
     def __init__(self, height, width, goal):
         self.grid = {}
         self.grid["grid"] = [[0] * height for i in range(width)]
+        
         self.height = height
         self.width = width
         self.goal = goal
-
-
+    # initialize grid at correct width and height
+    # self.grid = [[0 for x in range(height)] for y in range(width)]
+    # set player 1 to go first
+    
+    
     # check if grid has no spots left
     # returns true if full, false otherwise
     def checkIfFull(self):
@@ -143,27 +139,27 @@ class Board:
                 return False
         # otherwise, return true
         return True
-
-
+    
+    
     # calls checkVertical, checkHorizontal, and checkDiagonal
     # if any return true, return true. If not, return false
     def checkIfWon(self, newRowIndex, newColIndex, playerNum):
-        if self.checkVertical(newRowIndex, newColIndex, playerNum) or \
-                self.checkHorizontal(newRowIndex, newColIndex, playerNum) or \
-                self.checkDiagonal(newRowIndex, newColIndex, playerNum):
+        if self.checkVertical(newRowIndex, newColIndex, playerNum) or self.checkHorizontal(newRowIndex, newColIndex,
+                                                                                           playerNum) or self.checkDiagonal(
+                                                                                                                            newRowIndex, newColIndex, playerNum):
             return True
+                                                                                                                            return False
+
+
+# checks if there are 3 of that player's pieces under that player's last placed piece
+# note: don't need to check above because pieces can't be above it per the game rules
+def checkVertical(self, newRowIndex, newColIndex, playerNum):
+    below = self.checkDirection(newRowIndex, newColIndex, 0, 1, playerNum)
+    if below + 1 >= self.goal:
+        return True
         return False
-
-
-    # checks if there are 3 of that player's pieces under that player's last placed piece
-    # note: don't need to check above because pieces can't be above it per the game rules
-    def checkVertical(self, newRowIndex, newColIndex, playerNum):
-        below = self.checkDirection(newRowIndex, newColIndex, 0, 1, playerNum)
-        if below + 1 >= self.goal:
-            return True
-        return False
-
-
+    
+    
     # checks to the left and right of the piece, checks if there are 4 in a row horizontally
     def checkHorizontal(self, newRowIndex, newColIndex, playerNum):
         left = self.checkDirection(newRowIndex, newColIndex, -1, 0, playerNum)
@@ -174,26 +170,27 @@ class Board:
             # print("Hoizontal win")
             return True
         return False
-
-
+    
+    
     # calls checkLeftUp2DownRight and checkLeftDown2RightUp
     # if either returns true, return true, otherwise return false
     def checkDiagonal(self, newRowIndex, newColIndex, playerNum):
-        if self.checkLeftUp2RightDown(newRowIndex, newColIndex, playerNum) or \
-                self.checkLeftDown2RightUp(newRowIndex, newColIndex, playerNum):
+        if self.checkLeftUp2RightDown(newRowIndex, newColIndex, playerNum) or self.checkLeftDown2RightUp(newRowIndex,
+                                                                                                         newColIndex,
+                                                                                                         playerNum):
             return True
+                                                                                                         return False
+
+
+# checks to the left up and down right for matching pieces, checks if more than 4 in a row
+def checkLeftUp2RightDown(self, newRowIndex, newColIndex, playerNum):
+    leftUp = self.checkDirection(newRowIndex, newColIndex, -1, -1, playerNum)
+    rightDown = self.checkDirection(newRowIndex, newColIndex, 1, 1, playerNum)
+    if leftUp + rightDown + 1 >= self.goal:
+        return True
         return False
-
-
-    # checks to the left up and down right for matching pieces, checks if more than 4 in a row
-    def checkLeftUp2RightDown(self, newRowIndex, newColIndex, playerNum):
-        leftUp = self.checkDirection(newRowIndex, newColIndex, -1, -1, playerNum)
-        rightDown = self.checkDirection(newRowIndex, newColIndex, 1, 1, playerNum)
-        if leftUp + rightDown + 1 >= self.goal:
-            return True
-        return False
-
-
+    
+    
     # checks to the left down and right up for matching pieces, checks if more than 4 in a row
     def checkLeftDown2RightUp(self, newRowIndex, newColIndex, playerNum):
         leftDown = self.checkDirection(newRowIndex, newColIndex, 1, -1, playerNum)
@@ -201,8 +198,8 @@ class Board:
         if leftDown + rightUp + 1 >= self.goal:
             return True
         return False
-
-
+    
+    
     # takes a row index and column index and returns whether it is in bounds or not
     def inBounds(self, rowIndex, colIndex):
         if rowIndex < 0 or rowIndex > self.height - 1:
@@ -210,8 +207,8 @@ class Board:
         if colIndex < 0 or colIndex > self.width - 1:
             return False
         return True
-
-
+    
+    
     # make a move
     # this returns the row the piece was placed in
     def makeMove(self, column, playerNum):
@@ -225,14 +222,22 @@ class Board:
                 return row
 
 
-    # prints the board
-    # prints it as it should appear rather than in column-major format
-    def printBoard(self):
-        for row in range(0, self.height):
-            for column in range(0, self.width):
-                print(self.grid["grid"][column][row], end=" ")
+# prints the board
+# prints it as it should appear rather than in column-major format
+def printBoard(self):
+    for row in range(0, self.height):
+        for column in range(0, self.width):
+            print(self.grid["grid"][column][row], end=" ")
             print()
         print()
+
+
+    # # convert the board to a json
+    # def getBoardAsJSON(self):
+    #     js = self.grid
+    #
+    #     js = json.dumps(''.join(str(item) for sublist in js for item in sublist))
+    #     return js
 
 
     # colDir of -1 means to decrease the colIndex as you search
@@ -246,13 +251,13 @@ class Board:
             # check if out of bounds
             if self.inBounds(nextRowIndex, nextColIndex) == False:
                 return count
-                # if the spot is not the player whose turn it is
+            # if the spot is not the player whose turn it is
             if self.grid["grid"][nextColIndex][nextRowIndex] != playerNum:
                 # print(count)
                 return count
         # if it reaches here, have already found a win, return goal-1 which with the new spot is a win
         return self.goal - 1
-    
+
 def playTournament(height, width, goal, playerProgram1, playerProgram2):
     #results = [player1wins, player2wins, tie]
     results = [0,0,0]
@@ -271,8 +276,8 @@ def playTournament(height, width, goal, playerProgram1, playerProgram2):
     else:
         print("It is a tie")
 
-
-# playerfile = str(argv[0])
+p1error = open("player1error.txt", "w")
+p2error = open("player2error.txt", "w")
 
 height = 6
 width = 7
@@ -301,8 +306,10 @@ while(True):
     if int(mode) == 2:
         print("The current player program will be used for player 1.")
         playerProgram2 = input("Enter the player program you want to use for player 2")
+        p1error = open("player1error.txt", "w")
+        p2error = open("player2error.txt", "w")
         playTournament(height, width, goal, playerProgram, playerProgram2)
+        p1error.close()
+        p2error.close()
     if int(mode) == 3:
         break
-
-
